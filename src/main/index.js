4059337e00d26c95b0685d9a39c8334041b1f48f
Saibal Roy <saibal@technoexponent.com>
2021-02-19 20:14:12 +0530

    v0.0.19

--- src/main/index.js
+++ src/main/index.js
@@ -4,7 +4,7 @@ import { app, BrowserWindow } from 'electron'
 import * as path from 'path'
 import { format as formatUrl } from 'url'
 
-const appVersion = '0.0.18';
+const appVersion = '0.0.19';
 
 const log = require('electron-log');
 const {autoUpdater} = require("electron-updater");
